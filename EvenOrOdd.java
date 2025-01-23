884c324fce746b9e8b73cd9febf10ad295182b8e
prashu <prashucandy26@gmail.com>
2025-01-23 14:19:21 +0530

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,13 @@
+
+public class EvenOrOdd{
+	boolean isEvenOrOdd(int num) {
+		if(num%2==0) {
+			return true;
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		isEvenOrOdd(8);
+		
+	}
+}
